c0c2d536ba9200e3c58687aeb44894b2abec23e8
Jeffrey Salmons <jsalmons3@studnet.cscc.edu>
2026-06-20 13:54:19 -0400

    Made the introduction of the shop name players inventory and shop inventory

--- Shop_and_Inventory.py
+++ Shop_and_Inventory.py
@@ -5,3 +5,17 @@ A fantasy shop and inventory system for a game that uses a bunch of basic progra
 6/20/2026
 """
 
+player_name = input("What is your name, adventurer? ")
+print(f"Welcome to the Dragon's Hoard shop, {player_name}!")
+
+gold = 100
+
+player_inventory = ["Squire Sword", "Cloth Armour", "Wooden Shield"]
+
+shop = {
+    "Health Potion": 10,
+    "Master Sword": 200,
+    "Knight Armour": 75,
+    "Square Shield": 25,
+    "Ring of Strength": 50
+}
